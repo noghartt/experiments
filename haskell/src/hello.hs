382c3f7c95d7c8ef27b9d67956d936b28a1227d8
Guilherme Ananias <hi@noghartt.dev>
2024-06-01 10:57:32 -0300

    add megaparsec

--- haskell/src/hello.hs
+++ haskell/src/hello.hs
@@ -1,5 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-x = Just 3 >> Just 4
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void ( Void )
+
+newtype Identifier = Identifier
+  { getId :: String
+  } deriving (Show)
+
+data SExp
+  = SSExp SExp [SExp]
+  | SInteger Integer
+  | SString String
+  | SBool Bool
+  | SIdentifier Identifier
+  deriving (Show)
+
+type Parser = Parsec Void String
+
+bool :: Parser Bool
+bool = False <$ string "false" <|> True <$ string "true"
 
 main = putStrLn "Hello, World!"
